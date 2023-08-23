516392dd253e5a8032d1b19c2e6060d2357749fd
Maximilian Schmidt <maximilian@schmidt.so>
2023-08-23 21:46:53 +0200

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, Server};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().nest_service("/", ServeDir::new("assets/"));
+
+    Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
 }
