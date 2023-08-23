033fa9b6f92bf9350aeaac8678534cb98fb8e329
Maximilian Schmidt <maximilian@schmidt.so>
2023-08-23 22:53:51 +0200

    serve images

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use axum::{Router, Server};
+use clap::Parser;
 use tower_http::services::ServeDir;
+
+#[derive(Parser)]
+struct Arguments {
+    #[arg(long, default_value = "0.0.0.0")]
+    host: String,
+    #[arg(long, default_value = "3000")]
+    port: u16,
+    #[arg(long, default_value = "storage/")]
+    storage: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().nest_service("/", ServeDir::new("assets/"));
+    let arguments = Arguments::parse();
 
-    Server::bind(&"0.0.0.0:3000".parse()?)
-        .serve(app.into_make_service())
-        .await?;
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("assets/"))
+        .nest_service("/image", ServeDir::new(arguments.storage));
+
+    Server::bind(
+        &format!("{}:{}", arguments.host, arguments.port)
+            .parse()
+            .context("failed to parse host and port")?,
+    )
+    .serve(app.into_make_service())
+    .await
+    .context("failed to start server")?;
     Ok(())
 }
