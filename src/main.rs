fb54af4e146b00b2bad0aa2889b493fc4c5048d6
Maximilian Schmidt <maximilian@schmidt.so>
2023-09-18 08:49:38 +0200

    Multipart with NamedTempFile

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@ use anyhow::{Context, Result};
 use axum::{
     extract::{
         ws::{Message, WebSocket},
-        DefaultBodyLimit, Multipart, State, WebSocketUpgrade,
+        DefaultBodyLimit, State, WebSocketUpgrade,
     },
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Router, Server,
 };
+use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
 use clap::Parser;
 use index::Indexer;
+use tempfile::NamedTempFile;
 use time::{format_description::parse, OffsetDateTime};
-use tokio::{
-    fs::File,
-    io::{AsyncWriteExt, BufWriter},
-};
+use tokio::fs::copy;
 use tower_http::services::ServeDir;
 
 mod index;
@@ -57,7 +56,7 @@ async fn main() -> Result<()> {
             &format!("/upload/{}", arguments.secret),
             post(upload_image)
                 .with_state(configuration.clone())
-                .layer(DefaultBodyLimit::disable()),
+                .layer(DefaultBodyLimit::max(16 * 1024 * 1024)),
         )
         .fallback_service(ServeDir::new("frontend/"));
 
@@ -90,48 +89,30 @@ async fn handle_websocket(mut socket: WebSocket, indexer: Arc<Indexer>) {
     }
 }
 
+#[derive(TryFromMultipart)]
+struct UploadImageRequest {
+    #[form_data(limit = "unlimited")]
+    image: FieldData<NamedTempFile>,
+}
+
 // test with
 // curl --location "http://localhost:3000/upload/rohow2023" --form "image=@/my_picture.jpg"
 async fn upload_image(
     State(configuration): State<Arc<Configuration>>,
-    mut form_data: Multipart,
+    TypedMultipart(UploadImageRequest { image }): TypedMultipart<UploadImageRequest>,
 ) -> Result<(), (StatusCode, String)> {
-    while let Some(mut field) = form_data
-        .next_field()
+    let format = parse("[year][month][day]T[hour][minute][second]Z").unwrap();
+    let timestamp = OffsetDateTime::now_utc().format(&format).unwrap();
+    let file_name = image
+        .metadata
+        .file_name
+        .map(|file_name| format!("{timestamp}_{file_name}"))
+        .unwrap_or(timestamp);
+    let path = configuration.storage.join(file_name);
+    dbg!(&path);
+    dbg!(image.contents.path());
+    copy(image.contents.path(), &path)
         .await
-        .map_err(|error| (StatusCode::BAD_REQUEST, error.to_string()))?
-    {
-        let name = field
-            .name()
-            .ok_or_else(|| (StatusCode::BAD_REQUEST, "Missing field name".to_string()))?;
-        match name {
-            "image" => {
-                let format = parse("[year][month][day]T[hour][minute][second]Z").unwrap();
-                let timestamp = OffsetDateTime::now_utc().format(&format).unwrap();
-                let file_name = if let Some(file_name) = field.file_name() {
-                    format!("{timestamp}_{file_name}")
-                } else {
-                    timestamp
-                };
-                let path = configuration.storage.join(file_name);
-                let file = File::create(&path)
-                    .await
-                    .map_err(|error| (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))?;
-                let mut file = BufWriter::new(file);
-                while let Some(mut chunk) = field
-                    .chunk()
-                    .await
-                    .map_err(|error| (StatusCode::BAD_REQUEST, error.to_string()))?
-                {
-                    file.write_all_buf(&mut chunk)
-                        .await
-                        .map_err(|error| (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))?;
-                }
-            }
-            _ => {
-                eprintln!("ignoring field {name}");
-            }
-        }
-    }
+        .map_err(|error| (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))?;
     Ok(())
 }
